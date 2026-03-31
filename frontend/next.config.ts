e475006e467b9a086666f29eeb92e64c353e66ef
Rafli Pratama <rflipratm@gmail.com>
2026-03-31 20:12:58 +0700

    feat(all): setup agar bisa di akses sama ngrok

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*",
+      },
+      {
+        source: "/ai/:path*",
+        destination: "http://localhost:8001/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
